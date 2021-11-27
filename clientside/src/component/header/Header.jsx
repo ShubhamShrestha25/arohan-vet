dfb5093b6974cd4b48fe62d4d9faa8fc03389417
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-11-27 16:14:03 +0545

    Navbar

---
+++ clientside/src/component/header/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="left">
+        <img src="./images/arohan.png" alt="Arohan" />
+      </div>
+      <div className="right">
+        <ul>
+          <li> Home</li>
+          <li>About Us</li>
+          <li>Products</li>
+          <li>Activities</li>
+          <li>Gallery</li>
+          <li>News & notice</li>
+          <li>Related Links</li>
+          <li>Contact & Feedback</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ clientside/src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../component/header/Header";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Header />
+      <span className="span" />
+    </div>
+  );
+};
+
+export default Home;
